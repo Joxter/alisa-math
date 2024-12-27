80530cdf025275baea7079f26cc5bfd30adb75bb
Joxter <joxterrr@gmail.com>
2024-12-27 12:49:57 +0100

    add success modal

--- src/features/col-sum/Page.tsx
+++ src/features/col-sum/Page.tsx
@@ -1,7 +1,6 @@
 import { createEffect, createEvent, createStore, sample } from "effector";
 import { useUnit } from "effector-react";
 import css from "./Page.module.css";
-// import { ScrollDigit } from "./ScrollDigit.tsx";
 import React from "react";
 import { cn } from "../../utils.ts";
 
@@ -11,14 +10,13 @@ type Equation = {
   b: number;
 };
 
-type Answer = [number | null, number | null, number | null];
+type AnswerDigit = { n: number; status: null | "ok" | "error" };
+type Answer = [AnswerDigit | null, AnswerDigit | null, AnswerDigit | null];
 
 let $equation = createStore<Equation | null>(null);
 let $answer = createStore<Answer>([null, null, null]);
 
-// let answerChanged = createEvent<string | number | null>();
 let numClicked = createEvent<[number, 0 | 1 | 2]>();
-let backClicked = createEvent();
 
 let setEquation = createEvent<Equation>();
 let submitted = createEvent();
@@ -30,7 +28,7 @@ let submitFx = createEffect((data: { eq: Equation | null; answer: Answer }) => {
   if (!eq) {
     return { status: "error", message: "Не найден пример" } as const;
   }
-  let ans = answer.map((n) => (n === null ? "" : String(n))).join("");
+  let ans = answer.map((n) => (n === null ? "" : String(n.n))).join("");
   if (!ans) {
     return { status: "error", message: "Введите ответ" } as const;
   }
@@ -43,45 +41,58 @@ let submitFx = createEffect((data: { eq: Equation | null; answer: Answer }) => {
     return { status: "ok", message: "Ура! Это правильный ответ!" } as const;
   }
 
-  return { status: "error", message: "Неправильный ответ" } as const;
+  let correctAns = String(eq.a + eq.b);
+
+  let ansStatus = answer.map((d, i) => {
+    if (d === null) return null;
+    return d.n === +correctAns[i] ? "ok" : "error";
+  });
+
+  return { status: "error", message: "Неправильный ответ", ansStatus } as const;
 });
 
 $answer
-  // .on(answerChanged, (cur, answer) => {
-  //   if (answer === null) return null;
-  //
-  //   if (typeof answer === "string") {
-  //     let num = parseInt(answer);
-  //     if (Number.isNaN(num)) {
-  //       return String(num);
-  //     }
-  //     return null;
-  //   }
-  //
-  //   return cur;
-  // })
   .on(numClicked, (cur, [n, i]) => {
     let newAns = [...cur] as Answer;
-    newAns[i] = n;
+    newAns[i] = newAns[i]?.n === n ? null : { n, status: null };
     return newAns;
   })
   .on(newClicked, () => [null, null, null])
-  .on(submitFx.doneData, (_, res) => {
-    return res.status === "ok" ? [null, null, null] : _;
+  .on(submitFx.doneData, (current, res) => {
+    if (res.status === "error" && res.ansStatus) {
+      let newAns = [...current] as Answer;
+      res.ansStatus.forEach((status, i) => {
+        if (status) {
+          newAns[i] = { n: newAns[i]!.n, status };
+        }
+      });
+      return newAns;
+    }
   });
 
 $equation
   .on(setEquation, (_, equation) => equation)
   .on(newClicked, () => newEq())
   .on(submitFx.doneData, (_, res) => {
-    if (res.status === "ok") {
-      return newEq();
-    }
     return _;
+    // if (res.status === "ok") {
+    //   return newEq();
+    // }
   });
 
 submitFx.doneData.watch((res) => {
-  alert(res.message);
+  console.log(res.message);
+});
+
+let $modal = createStore(false);
+let setShowModal = createEvent<boolean>();
+
+$modal.on(setShowModal, (_, show) => show);
+
+submitFx.doneData.watch((res) => {
+  if (res.status === "ok") {
+    setShowModal(true);
+  }
 });
 
 sample({
@@ -93,7 +104,7 @@ sample({
 setEquation(newEq());
 
 export function ColSum() {
-  let [equation, answer] = useUnit([$equation, $answer]);
+  let [equation, answer, modal] = useUnit([$equation, $answer, $modal]);
 
   return (
     <form
@@ -103,6 +114,15 @@ export function ColSum() {
         submitted();
       }}
     >
+      {modal && (
+        <SuccessModal
+          onClose={() => {
+            setShowModal(false);
+            newClicked();
+          }}
+        />
+      )}
+
       {equation ? (
         <div className={css.equation}>
           <span className={css.plus}>+</span>
@@ -114,12 +134,17 @@ export function ColSum() {
       )}
       <hr />
       <div className={css.answer}>
-        {answer.map((n) => {
-          return <span>{n ?? "_"}</span>;
+        {answer.map((d) => {
+          let color = "";
+
+          if (d && d.status !== null) {
+            color = d.status === "ok" ? "" : "red";
+          }
+          return <span style={{ color }}>{d?.n ?? "_"}</span>;
         })}
       </div>
       <div className={css.numpad}>
-        {[0, 1, 3, 4, 5, 6, 7, 8, 9].map((n, i) => {
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((n, i) => {
           return (
             <React.Fragment key={i}>
               <NumpadNum
@@ -141,15 +166,6 @@ export function ColSum() {
           );
         })}
       </div>
-      {/*
-      <div>
-        <ScrollDigit
-          onChange={(n) => {
-            console.log("onChange", n);
-          }}
-        />
-      </div>
-      */}
       <div className={css.footer}>
         <button type="submit" className={css.niceButton}>
           Принять
@@ -170,7 +186,7 @@ function randomIntInRange(from: number, to: number) {
   return from + Math.floor(Math.random() * (to - from));
 }
 
-function newEq(): Equation {
+export function newEq(): Equation {
   let a = [
     //
     randomIntInRange(1, 3),
@@ -189,7 +205,7 @@ function newEq(): Equation {
 
 function NumpadNum(props: {
   val: number;
-  current: number | null;
+  current: AnswerDigit | null;
   onClick: () => void;
 }) {
   return (
@@ -197,7 +213,7 @@ function NumpadNum(props: {
       className={cn(
         //
         css.niceButton,
-        props.current === props.val && css.niceButtonCurrent,
+        props.current?.n === props.val && css.niceButtonCurrent,
       )}
       type="button"
       onClick={props.onClick}
@@ -207,6 +223,25 @@ function NumpadNum(props: {
   );
 }
 
-function Nbsp() {
-  return <span>&nbsp;</span>;
+type SuccessModalProps = {
+  onClose: () => void;
+};
+
+export function SuccessModal({ onClose }: SuccessModalProps) {
+  return (
+    <div className={css.modalOverlay}>
+      <div className={css.modalContent}>
+        <p>
+          Ура!
+          <br />
+          Это правильный ответ!
+        </p>
+        <div>
+          <button onClick={onClose} className={css.modalButton}>
+            Еще пример
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
