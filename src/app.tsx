42e15d996a37c451d892556c2c9c75821a491c6b
Joxter <joxterrr@gmail.com>
2025-01-04 07:23:26 +0100

    try spa 2

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
 import "./app.module.css";
-import { Route, Switch } from "wouter";
+import { Link, Route, Router, Switch } from "wouter";
 import { NotFound } from "./pages/NotFound.tsx";
 import { HomePage } from "./pages/HomePage.tsx";
+import { Layout } from "./pages/Layout.tsx";
+import { ColSum } from "./features/col-sum/Page.tsx";
 
 let prefix =
   location.origin === "https://joxter.github.io"
@@ -9,9 +11,12 @@ let prefix =
     : "/";
 
 function App() {
+  // basename
   return (
     <Switch>
       <Route path={prefix + ""} component={HomePage} />
+      <Route path={prefix + "/one"} component={One} />
+      <Route path={prefix + "/two"} component={Two} />
       <Route path={prefix + "*"}>
         <NotFound />
       </Route>
@@ -20,3 +25,23 @@ function App() {
 }
 
 export default App;
+
+export function One() {
+  return (
+    <Layout>
+      <p>ONE</p>
+      <Link href={prefix + "/one"}>to one</Link>
+      <Link href={prefix + "/two"}>to two</Link>
+    </Layout>
+  );
+}
+
+export function Two() {
+  return (
+    <Layout>
+      <p>TWO</p>
+      <Link href={prefix + "/one"}>to one</Link>
+      <Link href={prefix + "/two"}>to two</Link>
+    </Layout>
+  );
+}
